5b26862ebedf8f71999ebff394346c74aacc5a68
1ctinus <jtgc07@hotmail.com>
2021-01-25 19:12:55 -0600

    ready for beta

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // gets the json
 var custom = window.location.href.split("/");
 if (custom[4] != null) {
+    fetch("https://raw.githubusercontent.com/ozh/github-colors/master/colors.json").then(r => r.json()).then((color) => {console.log(color)
     fetch("https://api.github.com/repos/" + custom[3] + "/" + custom[4] + "/languages").then(r => r.json()).then((obj) => {
         // sums it up  
         console.log(obj);
@@ -8,7 +9,6 @@ if (custom[4] != null) {
             sum += obj[x];
 
         }
-
         console.log(sum)
         // gets the keys as an array
         let keys = Object.keys(obj);
@@ -16,14 +16,15 @@ if (custom[4] != null) {
         var i; var text = "";
         // array as brrrrrr
         for (i = 0; i < keys.length; i++) {
-            text += '<a class="d-inline-flex flex-items-center flex-nowrap link-gray no-underline text-small mr-3"><span class="text-gray-dark text-bold mr-1">' + keys[i] + '</span><span>' + (obj[Object.keys(obj)[i]] / sum * 100).toFixed(1) + '%</span></a>';
+            text += '<a class="d-inline-flex flex-items-center flex-nowrap link-gray no-underline text-small mr-3"><svg class="octicon octicon-dot-fill mr-2" style="color:'+color[ keys[i] ].color+';" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg><span class="text-gray-dark text-bold mr-1">' + keys[i] + '</span><span>' + (obj[Object.keys(obj)[i]] / sum * 100).toFixed(1) + '%</span></a>';
         }
         let node = document.createElement("LI");
         node.innerHTML = text;
         // let textnode = document.createTextNode(text);
         // node.appendChild(textnode);
-        document.getElementsByClassName('list-style-none')[document.getElementsByClassName("list-style-none").length - 3].appendChild(node);
+        document.getElementsByClassName('list-style-none')[document.getElementsByClassName("list-style-none").length - 3].innerHTML = text;
     })
+})
 }
     //<a class="d-inline-flex flex-items-center flex-nowrap link-gray no-underline text-small mr-3" href="/apache/superset/search?l=javascript" data-ga-click="Repository, language stats search click, location:repo overview">
     //<svg class="octicon octicon-dot-fill mr-2" style="color:#f1e05a;" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
